b4001b1cc45940db34c7cbd06950c62ab3eac0cb
Rahul <rahulshermale50@gmail.com>
2023-12-31 00:56:50 +0530

    Start Props createindex Compoenent

--- src/App.js
+++ src/App.js
@@ -1,61 +1,17 @@
 import React from 'react';
 import './App.css';
-// import images from images;
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Component/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const projects = [
-  {
-    title: 'Project 1',
-    description: 'Description of Project 1.',
-    image: 'project1.jpg',
-  },
-  {
-    title: 'Project 2',
-    description: 'Description of Project 2.',
-    image: 'project1.jpg',
-  },
-  // Add more projects as needed
-];
+
 
 function App() {
-  return (<nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+  return (<>
+  <Navbar title="Rahul" Home="Home" about="About" />
+  
+  
+  </>
    
   );
 }
